f1154aa435a51a037bf7ce35f5187a32fd419175
nohara <nohara@sinap.jp>
2020-01-29 23:33:50 +0900

    カテゴリコンポーネント追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CategorySelector />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CategorySelector from "./components/category-selector.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    CategorySelector
   }
 };
 </script>
---
+++ src/dummy/categories.js
@@ -0,0 +1,90 @@
+export const dummyCategories = [
+  {
+    code: "RSFST09000",
+    name: "居酒屋"
+  },
+  {
+    code: "RSFST02000",
+    name: "日本料理・郷土料理"
+  },
+  {
+    code: "RSFST03000",
+    name: "すし・魚料理・シーフード"
+  },
+  {
+    code: "RSFST04000",
+    name: "鍋"
+  },
+  {
+    code: "RSFST05000",
+    name: "焼肉・ホルモン"
+  },
+  {
+    code: "RSFST06000",
+    name: "焼き鳥・肉料理・串料理"
+  },
+  {
+    code: "RSFST01000",
+    name: "和食"
+  },
+  {
+    code: "RSFST07000",
+    name: "お好み焼き・粉物"
+  },
+  {
+    code: "RSFST08000",
+    name: "ラーメン・麺料理"
+  },
+  {
+    code: "RSFST14000",
+    name: "中華"
+  },
+  {
+    code: "RSFST11000",
+    name: "イタリアン・フレンチ"
+  },
+  {
+    code: "RSFST13000",
+    name: "洋食"
+  },
+  {
+    code: "RSFST12000",
+    name: "欧米・各国料理"
+  },
+  {
+    code: "RSFST16000",
+    name: "カレー"
+  },
+  {
+    code: "RSFST15000",
+    name: "アジア・エスニック料理"
+  },
+  {
+    code: "RSFST17000",
+    name: "オーガニック・創作料理"
+  },
+  {
+    code: "RSFST10000",
+    name: "ダイニングバー・バー・ビアホール"
+  },
+  {
+    code: "RSFST21000",
+    name: "お酒"
+  },
+  {
+    code: "RSFST18000",
+    name: "カフェ・スイーツ"
+  },
+  {
+    code: "RSFST19000",
+    name: "宴会・カラオケ・エンターテイメント"
+  },
+  {
+    code: "RSFST20000",
+    name: "ファミレス・ファーストフード"
+  },
+  {
+    code: "RSFST90000",
+    name: "その他の料理"
+  }
+];
